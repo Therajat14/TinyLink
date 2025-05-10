96e7367977b9651ce092b560a26e141ed826c7f3
therajat14 <heylegend14@gmail.com>
2025-05-10 15:16:39 +0530

    Removed logs

--- server/server.js
+++ server/server.js
@@ -15,6 +15,4 @@ app.use(express.json());
 
 app.use("/", routes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on port " + process.env.PORT);
-});
+app.listen(process.env.PORT);
